56943ad4d4330d78ebf93685389a8a7f9c1bccb1
victornavelino <victornavelino@gmail.com>
2021-12-30 13:28:00 -0300

    subiendo usuario index y cambios en controller

--- src/main/java/com/springboot/web/controllers/IndexController.java
+++ src/main/java/com/springboot/web/controllers/IndexController.java
@@ -1,14 +1,30 @@
 package com.springboot.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springboot.web.models.Usuario;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index", "/", "/home"})
-	public String index(){
+	@GetMapping({"/index", "/","", "/home"})
+	public String index(Model model){
 		
+		model.addAttribute("titulo", "Hola SpringBoot");
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Elian");
+		usuario.setApellido("Navelino");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del Usuario: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
 }
